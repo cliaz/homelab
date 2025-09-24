f26724cc294aa40c6ac18986c2b4285c8398fc97
cliaz <3636531+cliaz@users.noreply.github.com>
2025-09-24 20:30:35 +1000

    Clarification, modular test script

--- scripts/f1_sonarr_importer_config.py
+++ scripts/f1_sonarr_importer_config.py
@@ -6,11 +6,17 @@ TARGET_SEASON = 2025
 ALLOW_PARTIAL_MATCHING = False  # This will allow partial matching of Session (Practice / Qualifying / Race) names in the downloaded file
                                 # to help with getting a match. May experience unwanted matches, eg "Ted's Qualifying Notebook" matching to
                                 # the "Qualifying" session
+
+## Used for running the script manually from the host system. Paths here are as the HOST machine sees them.
 F1_DOWNLOAD_DIR = "/media/MEDIA_SSD/torrents/f1"    # where the F1 files are downloaded to, as the host machine sees it
 TARGET_IMPORT_DIR = "/media/MEDIA_SSD/torrents/tv"  # where the F1 files should be moved to, aka the downloads folder that Sonarr is watching
-                                                    # this is the host machine path, NOT the Sonarr container path
-SONARR_TARGET_IMPORT_DIR = "/data/torrents/tv"      # IMPORTANT: this is the Sonarr container path to the above folder
+                                                    
+
+## Used for running the script from within a container. Paths here are as the CONTAINER sees them.
+SONARR_TARGET_IMPORT_DIR = "/data/torrents/tv"      # IMPORTANT: this is the Sonarr container path to TARGET_IMPORT_DIR folder.
                                                     # usually /data/torrents/tv if you followed the TRaSH Guide.
+QBITTORRENT_DOWNLOAD_DIR = "/data/torrents/"        # IMPORTANT: this is the qBittorrent container path to where torrents are downloaded
+                                                    # usually /data/torrents/ if you followed the TRaSH Guide.
 
 # Example: 
 # For my setup, I have /media/MEDIA_SSD/ mounted as /data in the Sonarr container, shown in a snippet from my docker-compose.yml:
